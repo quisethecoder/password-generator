e3a1b6d37fd01ffff208c4cd72f010358f188235
quisethecoder <marquise_dawkins@aol.com>
2023-02-04 17:32:37 -0500

    password randomizer

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-const lengthSlider = document.querySelector(".pass-length input");
+const lengthSlider = document.querySelector(".pass-length input"),
+options = document.querySelectorAll(".option input"),
+generateBtn = document.querySelector(".generate-btn");
+
+const characters = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers: "0123456789",
+    symbols: "~!@#$%^&*()_+{}[]|\:;<>,./?"
+}
+
+const generatePassword = () => {
+    let staticPassword = "",
+    randomPassword = "",
+    passLength = lengthSlider.value;
+
+    options.forEach(options => {
+        if(options.checked){
+            
+            staticPassword += characters[options.id];
+        }
+    });
+
+    for(let i = 0; i < passLength; i++){
+        randomPassword += staticPassword[Math.floor(Math.random() * staticPassword.length)];
+    }
+
+    console.log(randomPassword)
+}
 
 const updateSlider = () => {
     document.querySelector(".pass-length span").innerText = lengthSlider.value;
 }
 
 lengthSlider.addEventListener("input", updateSlider);
+generateBtn.addEventListener("click", generatePassword);
