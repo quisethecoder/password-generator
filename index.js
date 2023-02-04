5b9c5f50bab7f18ed39bc8e03406613dd0b77a55
quisethecoder <marquise_dawkins@aol.com>
2023-02-04 17:09:28 -0500

    checkbox function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const lengthSlider = document.querySelector(".pass-length input");
 
 const updateSlider = () => {
-    document.querySelector(".pass-length span").innerText = lengthSlider.ariaValueMax;
+    document.querySelector(".pass-length span").innerText = lengthSlider.value;
 }
 
 lengthSlider.addEventListener("input", updateSlider);
