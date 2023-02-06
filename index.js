a4607ac075faee06fce2cedffc454394563372df
quisethecoder <marquise_dawkins@aol.com>
2023-02-06 13:44:07 -0500

    copy icon addition

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const lengthSlider = document.querySelector(".pass-length input"),
 options = document.querySelectorAll(".option input"),
+copyIcon = document.querySelectorAll("input-box span"),
 passwordInput = document.querySelector(".input-box input"),
 passIndicator = document.querySelector(".pass-indicator"),
 generateBtn = document.querySelector(".generate-btn");
